a5ddc924f343f0262c681f4e942e76de51afb1d8
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2020-11-05 12:41:31 +0500

    fonts update

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,48 +16,97 @@ module.exports = {
                 }
             },
             fontFamily: {
+                'sans': ['Product'],
                 'lemon': ['Lemon', 'sans-serif'],
             },
             keyframes: {
                 slideup: {
-                    '0%'   : { transform: 'translateY(70%)'},
-                    '100%' : { transform: 'translateY(0%)' },
+                    '0%': {
+                        transform: 'translateY(70%)'
+                    },
+                    '100%': {
+                        transform: 'translateY(0%)'
+                    },
                 },
                 slidedown: {
-                    '0%'   : { transform: 'translateY(-70%)'},
-                    '100%' : { transform: 'translateY(0%)' },
+                    '0%': {
+                        transform: 'translateY(-70%)'
+                    },
+                    '100%': {
+                        transform: 'translateY(0%)'
+                    },
                 },
                 slideleft: {
-                    '0%'   : { transform: 'translateX(70%)'},
-                    '100%' : { transform: 'translateX(0%)' },
+                    '0%': {
+                        transform: 'translateX(70%)'
+                    },
+                    '100%': {
+                        transform: 'translateX(0%)'
+                    },
                 },
                 slideright: {
-                    '0%'   : { transform: 'translateX(-70%)'},
-                    '100%' : { transform: 'translateX(0%)' },
+                    '0%': {
+                        transform: 'translateX(-70%)'
+                    },
+                    '100%': {
+                        transform: 'translateX(0%)'
+                    },
                 },
                 slideup_fadein: {
-                    '0%'   : { opacity: 0, transform: 'translateY(70%)'},
-                    '100%' : { opacity: 1, transform: 'translateY(0%)' },
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateY(70%)'
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateY(0%)'
+                    },
                 },
                 slidedown_fadein: {
-                    '0%'   : { opacity: 0, transform: 'translateY(-70%)'},
-                    '100%' : { opacity: 1, transform: 'translateY(0%)' },
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateY(-70%)'
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateY(0%)'
+                    },
                 },
                 slideleft_fadein: {
-                    '0%'   : { opacity: 0, transform: 'translateX(70%)'},
-                    '100%' : { opacity: 1, transform: 'translateX(0%)' },
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateX(70%)'
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateX(0%)'
+                    },
                 },
                 slideright_fadein: {
-                    '0%'   : { opacity: 0, transform: 'translateX(-70%)'},
-                    '100%' : { opacity: 1, transform: 'translateX(0%)' },
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateX(-70%)'
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateX(0%)'
+                    },
                 },
-                fadein :{
-                    '0%'   : { opacity: 0 },
-                    '100%' : { opacity: 1 },
+                fadein: {
+                    '0%': {
+                        opacity: 0
+                    },
+                    '100%': {
+                        opacity: 1
+                    },
                 },
-                fadeout :{
-                    '0%'   : { opacity: 1 },
-                    '100%' : { opacity: 0 },
+                fadeout: {
+                    '0%': {
+                        opacity: 1
+                    },
+                    '100%': {
+                        opacity: 0
+                    },
                 },
             },
             animation: {
@@ -65,10 +114,10 @@ module.exports = {
                 slidedown: 'slidedown 1s ease-out',
                 slideleft: 'slideleft 1s ease-out',
                 slideright: 'slideright 1s ease-out',
-                slideup_fadein: 'slideup_fadein 1s ease-out', 
-                slidedown_fadein: 'slidedown_fadein 1s ease-out', 
-                slideleft_fadein: 'slideleft_fadein 1s ease-out', 
-                slideright_fadein: 'slideright_fadein 1s ease-out', 
+                slideup_fadein: 'slideup_fadein 1s ease-out',
+                slidedown_fadein: 'slidedown_fadein 1s ease-out',
+                slideleft_fadein: 'slideleft_fadein 1s ease-out',
+                slideright_fadein: 'slideright_fadein 1s ease-out',
                 fadein: 'fadein 1s ease-out',
                 fadeout: 'fadeout 1s ease-out',
             },
